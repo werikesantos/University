06f826a4285cb3d65dc70735f07403fde241b453
Werikesantos <werike.ms@gmail.com>
2020-04-14 00:11:32 -0300

    Aula 07 - ArrayList

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/ArrayList_07/src/aula07_00/Cliente.java
@@ -0,0 +1,40 @@
+package aula07_00;
+
+//Esta classe modela o cliente que irá fazer compras no "emporium".
+
+public class Cliente {
+	
+	// atributos: armazenam os dados de um objeto, após este ser instanciado.
+	
+	private String nome;
+	private String cpf;
+
+	/* método construtor: usado para inicializar os atributos,
+	 * quando um objeto cliente é criado (instanciado). Notar que o construtor
+	 * faz chamada interna de dois métodos modificadores.
+	 */
+	
+	public Cliente(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	// métodos de acesso: usados para retornar o valor de um atributo.
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	// métodos modificadores: usados para alterar o valor de um atributo, depois que o objeto foi criado (instanciado).
+	public void setNome(String novoNome) {
+		nome = novoNome;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
