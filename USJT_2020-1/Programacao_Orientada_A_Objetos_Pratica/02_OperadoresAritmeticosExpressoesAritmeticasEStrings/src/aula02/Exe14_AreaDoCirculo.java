8928cbcd478cba1a6334455e5ae03b776a3f4cdb
Werikesantos <werike.ms@gmail.com>
2020-05-25 15:50:01 -0300

    Exe14_AreaDoCirculo

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/Exe14_AreaDoCirculo.java
@@ -0,0 +1,24 @@
+package aula02;
+
+import javax.swing.JOptionPane;
+
+public class Exe14_AreaDoCirculo {
+
+	public static void main(String[] args) {
+		
+		/* EXERCICIO 14
+		 * 
+		 * Crie um algoritmo para calcular a área de um circulo, com base no seu raio 
+		 * (área = PI*raio*raio, onde PI = 3.14159). Use variáveis reais.
+		 * 
+		 */
+		
+		String sRaio = JOptionPane.showInputDialog("Digite o valor do raio:");
+		
+		double raio = Double.parseDouble(sRaio);
+		double PI = 3.14159;
+		double area = (PI * Math.pow(raio, 2));
+
+		JOptionPane.showMessageDialog(null, "A área de um circulo, com base no seu raio é igual:\n\n"+area);
+	}
+}
