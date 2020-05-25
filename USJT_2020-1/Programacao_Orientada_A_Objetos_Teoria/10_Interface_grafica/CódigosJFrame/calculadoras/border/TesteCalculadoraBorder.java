049b0d80dd69f2552dc69d761d60112a37434784
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:33:26 -0300

    Classe TesteCalculadoraBorder

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/10_Interface_grafica/CódigosJFrame/calculadoras/border/TesteCalculadoraBorder.java
@@ -0,0 +1,14 @@
+package exercicio.border;
+import javax.swing.JFrame;
+
+import exercicio.calculadora.Calculadora;
+
+public class TesteCalculadoraBorder {
+	
+	public static void main(String args[]){
+		Calculadora calculadora = new Calculadora();
+		CalculadoraBorder tela = new CalculadoraBorder(calculadora);
+		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	} // Fecha o método main()
+}
+
