b99c546b434d3e294da4cbdfab2449b40dd29bfd
Werikesantos <werike.ms@gmail.com>
2020-05-25 15:49:38 -0300

    Exe12_Substring

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/Exe12_Substring.java
@@ -0,0 +1,26 @@
+package aula02;
+
+import javax.swing.JOptionPane;
+
+public class Exe12_Substring {
+
+	public static void main(String[] args) {
+			
+		/* EXERCICIO 12
+		 * 
+		 * Entre com uma data em uma variável do tipo String no formato dd/mm/aa e imprimir
+		 * dia, mês e ano separados.
+		 * 
+		 */
+		
+		String sData = JOptionPane.showInputDialog("Digite o dia, mês e ano seguindo o exemplo:\n(dd/mm/aaaa)");
+		
+		String dia = (sData.substring(0,2));
+		String mes = (sData.substring(3,5));
+		String ano = (sData.substring(6,10));
+
+		JOptionPane.showMessageDialog(null, "Dia: "+dia+"\nMês: "+mes+"\nAno: "+ano);
+
+	}
+
+}
