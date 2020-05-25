8524e26ec083d441bb6f9b52e5fa5a4a062dc47c
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:36:57 -0300

    Classe CalculadoraGridCombo

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/10_Interface_grafica/CódigosJFrame/codigo aula teoria/CalculadoraGridCombo.java
@@ -0,0 +1,7 @@
+public class CalculadoraGridCombo{
+
+   public static void main(String[] args){
+      //instancia o JFrame
+      TelaCalculadoraCombo tela = new TelaCalculadoraCombo();      
+   }
+} 
