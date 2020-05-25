43b3caa105ff87cd1a0be785ac95ba0e1cb37dd2
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:06:32 -0300

    Classe Carrinho

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/07_ArrayList_/src/aula07_00/Carrinho.java
@@ -0,0 +1,13 @@
+package aula07_00;
+
+public class Carrinho {
+	
+	//MÉTODO COLOCAR NO CARRINHO
+	public void colocarProdutoNoCarrinho(String nomeProduto, double precoUnitario, double quantidade) {
+		
+		
+	}
+	
+	
+	
+}
