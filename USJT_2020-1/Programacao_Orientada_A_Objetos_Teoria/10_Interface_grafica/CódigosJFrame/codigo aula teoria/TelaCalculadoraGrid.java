340b9a50b5ab71abc7c6806f3ed0d4133d8f621b
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:43:11 -0300

    Classe TelaCalculadoraGrid

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/10_Interface_grafica/CódigosJFrame/codigo aula teoria/TelaCalculadoraGrid.java
@@ -0,0 +1,49 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TelaCalculadoraGrid extends JFrame implements ActionListener{
+
+   private JButton botao; // botao
+   private JTextField texto; //campo texto de uma linha
+   private JLabel etiqueta; //etiqueta de nome do campo
+   
+   public TelaCalculadoraGrid(){
+   //chamar construtor da superclasse e configurar o titulo
+      super("Calculadora");
+      //instanciar os widgets
+      botao = new JButton("Soma");
+      texto = new JTextField("0", 10);
+      etiqueta = new JLabel("Valor: ");
+      //pega o container (ou painel)
+      Container caixa = getContentPane();
+      //configura o gerenciador de layout
+      caixa.setLayout(new GridLayout(3,1));
+      //adiciona na tela na ordem em que quer que apareca
+      caixa.add(etiqueta);
+      caixa.add(texto);
+      caixa.add(botao);
+      //registra este objeto como listener
+      botao.addActionListener(this);
+      //configura ajustes finais
+      //configura o tamanho inicial da tela
+      setSize(200,100);
+      //encerra a aplicacao quando clica o xis
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //torna a janela visivel
+      setVisible(true); 
+   }
+   
+   public void actionPerformed(ActionEvent e){
+      if(e.getSource()==botao){
+         int valor = Integer.parseInt(texto.getText());
+         valor+=10;
+         texto.setText(""+valor);
+      }
+   }
+}
