010e3fba0e96a8a1a79bb4533a0a04bbbf75dcab
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:48:13 -0300

    Classe TelaGrid

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/10_Interface_grafica/CódigosJFrame/teoria17/TelaGrid.java
@@ -0,0 +1,55 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class TelaGrid extends JFrame implements ActionListener{
+   private JLabel lblValor;
+   private JTextField txtValor;
+   private JButton btnSoma, btnSair;
+   
+   
+   public TelaGrid(){
+      super("Calculadora Grid");
+      //instanciar elementos de interface (widgets)
+      lblValor = new JLabel("Valor:");
+      txtValor = new JTextField(10);
+      btnSoma = new JButton("Soma 10");
+      btnSair = new JButton("Sair");
+      
+      //acesso ao canvas
+      Container tela = getContentPane();
+      tela.setLayout(new GridLayout(2, 2));
+      
+      //colocar no canvas
+      add(lblValor);
+      add(txtValor);
+      add(btnSoma);
+      add(btnSair);
+      
+      //configurar listeners
+      btnSoma.addActionListener(this);
+      btnSair.addActionListener(this);
+   
+      //ajustes finais
+      setLocationRelativeTo(null);
+      setSize(220, 100);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+   }
+   
+   public void actionPerformed(ActionEvent ae){
+      if(ae.getSource() == btnSoma){
+         int valor = Integer.parseInt(txtValor.getText());
+         valor += 10;
+         txtValor.setText(""+valor);
+      } else if(ae.getSource() == btnSair){
+         System.exit(0);
+      }
+   }
+}
