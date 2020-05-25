0bade8ec0ba43843d6199b057872a7bf26fcab3f
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:18:37 -0300

    Exe10_ConverterBoolean

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe10_ConverterBoolean.java
@@ -0,0 +1,17 @@
+package aula01;
+
+import javax.swing.JOptionPane;
+
+public class Exe10_ConverterBoolean {
+
+	public static void main(String[] args) {
+
+		// CONVERTENDO A STRING PARA UM BOOLEAN:
+		boolean z = Boolean.parseBoolean(JOptionPane.showInputDialog("Digite 'true' ou 'false':"));
+
+		// MOSTRANDO O RESULTADO:
+		JOptionPane.showMessageDialog(null, "Resultado: " + z);
+
+	}
+
+}
