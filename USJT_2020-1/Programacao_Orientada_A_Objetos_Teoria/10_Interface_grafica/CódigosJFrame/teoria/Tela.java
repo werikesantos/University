4b298c9089d10b2b10ab9502ecd9cc9155cb49ca
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:47:10 -0300

    Classe Tela

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/10_Interface_grafica/CódigosJFrame/teoria/Tela.java
@@ -0,0 +1,54 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import java.awt.Container;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Tela extends JFrame implements ActionListener{
+   private JTextField txtValor;
+   private JLabel lblValor;
+   private JButton btnSoma, btnFechar;
+
+   public Tela(){
+      //configura o titulo
+      super("Calculadora");
+      //instanciar elementos de interface
+      lblValor = new JLabel("Valor:");
+      txtValor = new JTextField(10);
+      btnSoma = new JButton("Soma 10");
+      btnFechar = new JButton("Fechar");
+      //configura o gerenciador de layout
+      Container caixa = getContentPane();
+      caixa.setLayout(new FlowLayout());
+      //adicionar a tela
+      caixa.add(lblValor);
+      caixa.add(txtValor);
+      caixa.add(btnSoma);
+      caixa.add(btnFechar);
+      //configurar eventos
+    //  btnSoma.addActionListener(this);
+      btnFechar.addActionListener(this);
+      //retoques finais
+      setSize(224,94);
+      setLocationRelativeTo(null);
+      setVisible(true);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+   
+   public void actionPerformed(ActionEvent e){
+      if(e.getSource()==btnSoma){
+         String sValor = txtValor.getText();
+         int valor = Integer.parseInt(sValor) + 10;
+         txtValor.setText(""+valor);       
+      } else if (e.getSource()==btnFechar){
+         System.exit(0);
+      }
+   }
+
+   public static void main(String[] args){
+      new Tela();
+   }
+}
