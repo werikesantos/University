d0c31ce0e5743f3013633da0b89b28315ee4f944
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:19:44 -0300

    Exe01_PedacoString

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/02_Revisao_aritmeticos_string/src/aula02/Exe01_PedacoString.java
@@ -0,0 +1,16 @@
+package aula02;
+
+public class Exe01_PedacoString {
+
+	public static void main(String[] args) {
+		
+		//PARA PEGARUM PEDAÇO DA STRING UTILIZE O MÉTODO '.substring(0,1)'
+		//NOTE QUE DENTRO DOS PARÊNTESES EXISTEM DOIS VALORES.
+		//O PRIMEIRO INDICA O INÍCIO E O SEGUNDO INDICA O FIM DA STRING.
+		String s = "WERIKE MARQUES SANTOS".substring(0,6);
+		
+		System.out.println(s);
+
+	}
+
+}
