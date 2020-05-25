b4a111c97677b6cb372b442cf010409fa3e3df20
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:22:29 -0300

    Exe02_WhilePrintAteDez

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/04_Lacos_de_repeticao/src/aula04/Exe02_WhilePrintAteDez.java
@@ -0,0 +1,21 @@
+package aula04;
+
+public class Exe02_WhilePrintAteDez {
+
+	public static void main(String[] args) {
+
+		//IMPRIMINDO DE 1 A 10
+		
+		int i = 0;
+		
+		//'WHILE' QUER DIZER 'ENQUANTO'
+		while (i <= 10) {
+			
+			System.out.println(i);
+			i++;
+			
+		}
+
+	}
+
+}
