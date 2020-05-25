b3ccdbf3aeee9097cfd16336417035fb026cfe30
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:18:04 -0300

    Exe05_JOptionPaneDialogo

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe05_JOptionPaneDialogo.java
@@ -0,0 +1,18 @@
+package aula01;
+
+//IMPORTANDO A CLASE RESPONSÁVEL PELO MÉTODO 'JOptionPane'
+import javax.swing.JOptionPane;
+
+public class Exe05_JOptionPaneDialogo {
+
+	public static void main(String[] args) {
+		
+		//MOSTRA UMA CAIXA DE DIÁLOGO QUE COLETA DADOS
+		String z = JOptionPane.showInputDialog("Digite seu nome:");
+		
+		//MOSTRANDO O CONTEÚDO GUARDADO NA CAIXA DE DIÁLOGO NO CONSOLE:
+		System.out.println(z);
+		
+	}
+
+}
