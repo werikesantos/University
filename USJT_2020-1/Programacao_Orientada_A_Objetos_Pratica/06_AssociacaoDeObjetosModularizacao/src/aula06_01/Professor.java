f8b9f174695c06ebf9df5c77a017d47043b46ae5
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:04:59 -0300

    Classe Professor

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/06_AssociacaoDeObjetosModularizacao/src/aula06_01/Professor.java
@@ -0,0 +1,36 @@
+package aula06_01;
+
+public class Professor {
+	
+	private String nome;
+	private int idade;
+	
+	//CONSTRUTOR
+	public Professor(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	//SETTER
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	//GETTER
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+	
+	//MÉTODOS
+	public String getDados() {
+		return ("Nome: "+getNome()+"\n"+"Idade: "+getIdade()+"\n");
+	}
+}
