0a4c907259931043cdec7245d72af2c2cee6cc80
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:03:09 -0300

    Classe Caneta

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/05_IntroducaoOrientacaoObjetosOO/src/materialAula05/Caneta.java
@@ -0,0 +1,36 @@
+package materialAula05;
+
+public class Caneta
+{
+   private  String   tipo;
+   private  String   cor;
+   
+   public   Caneta()
+   {
+      tipo = ""; 
+      cor  = "";
+   }
+   
+   public   Caneta(String t, String c)
+   {
+      tipo = t;
+      cor  = c;
+   }
+   
+   public   void  setTipo(String t)
+   {
+      tipo = t;
+   }
+   public   String   getTipo()
+   {
+      return   tipo;
+   }
+   public   void  setCor(String c)
+   {
+      cor = c;
+   }
+   public   String   getCor()
+   {
+      return   cor;
+   }
+}
