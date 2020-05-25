2c62ab77d6dacdc421e76af97f12ab20acdf93ad
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:18:17 -0300

    Exe07_Println

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe07_Println.java
@@ -0,0 +1,16 @@
+package aula01;
+
+public class Exe07_Println {
+
+	public static void main(String[] args) {
+		
+		int x = 8;
+		int y = 1993;
+		//EXEMPLO DE EXIBIÇÃO NO CONSOLE
+		System.out.println("Erro do sistema");
+		
+		System.out.println("Soma = " + (x + y) + " reais.");
+
+	}
+
+}
