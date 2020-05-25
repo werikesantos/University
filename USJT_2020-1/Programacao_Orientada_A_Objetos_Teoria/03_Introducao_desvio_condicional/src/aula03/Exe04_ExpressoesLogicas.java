ed9fac16fb40f842cc957f3c8b515f835c48c2e0
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:21:06 -0300

    Exe04_ExpressoesLogicas

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/03_Introducao_desvio_condicional/src/aula03/Exe04_ExpressoesLogicas.java
@@ -0,0 +1,17 @@
+package aula03;
+
+public class Exe04_ExpressoesLogicas {
+
+	public static void main(String[] args) {
+
+		/* EXEMPLOS DE EXPRESSÕES LÓGICAS:
+		 * 
+		 * && E
+		 * || OU
+		 * ! NÃO
+		 * 
+		 */
+
+	}
+
+}
