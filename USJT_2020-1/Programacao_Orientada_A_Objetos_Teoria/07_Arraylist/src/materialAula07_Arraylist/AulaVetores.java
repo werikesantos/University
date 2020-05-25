cdcb9eaf6b73458e8c5b90f4308b30bb897b1ac3
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:26:37 -0300

    AulaVetores

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/07_Arraylist/src/materialAula07_Arraylist/AulaVetores.java
@@ -0,0 +1,24 @@
+package materialAula07_Arraylist;
+import java.util.Scanner;
+
+public class AulaVetores {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int[] vetor = new int[4];
+		
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.println("Digite o "+(i+1)+" valor");
+			vetor[i] = entrada.nextInt();
+	    }
+		
+		System.out.println("O valores armazenados no vetor são \n");
+		for(int i = 0; i < vetor.length; i++)
+		{
+			System.out.println(vetor[i]);
+		}
+		
+		System.out.println("O valor armazenado ona posição 02 do vetor é: "+vetor[2]);
+	}	
+
+}
