d719a4e1b3060fc95816ba1a0dab6e67cee427c0
Werikesantos <werike.ms@gmail.com>
2020-05-25 15:47:27 -0300

    Exe00_imc

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/Exe00_imc.java
@@ -0,0 +1,25 @@
+package aula02;
+
+import javax.swing.JOptionPane;
+
+public class Exe00_imc {
+
+	public static void main(String[] args) {
+		
+		//lendo os dados
+		String sPeso = JOptionPane.showInputDialog("Digite seu peso em kilogramas:");
+		String sAltura = JOptionPane.showInputDialog("Digite sua altura em metros:");
+		
+		//convertendo para reais
+		double peso = Double.parseDouble(sPeso);
+		double altura = Double.parseDouble(sAltura);
+		
+		//calculando o imc
+		double imc = peso / (altura * altura);
+		
+		//apresentando o resultado
+		JOptionPane.showMessageDialog(null, "IMC = " + imc);
+
+	}
+
+}
