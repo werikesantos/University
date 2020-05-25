44615cc8c5c589699ad4e9d3917b3c1675fc7e04
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:36:35 -0300

    Classe CalculadoraBorder

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/10_Interface_grafica/CódigosJFrame/codigo aula teoria/CalculadoraBorder.java
@@ -0,0 +1,7 @@
+public class CalculadoraBorder{
+
+   public static void main(String[] args){
+      //instancia o JFrame
+      TelaCalculadoraBorder tela = new TelaCalculadoraBorder();      
+   }
+} 
