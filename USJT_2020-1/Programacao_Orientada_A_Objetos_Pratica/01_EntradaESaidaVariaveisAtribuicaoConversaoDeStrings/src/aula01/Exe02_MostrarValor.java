1669480b3efe11b8801521a2ad2120356460139b
Werikesantos <werike.ms@gmail.com>
2020-05-25 15:42:16 -0300

    Exe02_MostrarValor

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/01_EntradaESaidaVariaveisAtribuicaoConversaoDeStrings/src/aula01/Exe02_MostrarValor.java
@@ -0,0 +1,27 @@
+package aula01;
+
+import javax.swing.JOptionPane;
+
+public class Exe02_MostrarValor {
+
+	public static void main(String[] args) {
+		
+		/* EXERCICIO 02
+		 * 
+		 * Crie um algoritmo que leia um número inteiro, armazene-o em uma variável do tipo int e
+		 * depois escreva na tela: O valor do número digitado é: e o valor do número.
+		 * 
+		 */
+		
+		//Solicita ao usuario que digite um valor
+		String sNumero = JOptionPane.showInputDialog("Digite um número:");
+		
+		//Convertendo a variavel String para int
+		int numero = Integer.parseInt(sNumero);
+		
+		//Exibindo ao usuario o resultado
+		JOptionPane.showMessageDialog(null, "O valor do número digitado é: " + numero);
+
+	}
+
+}
