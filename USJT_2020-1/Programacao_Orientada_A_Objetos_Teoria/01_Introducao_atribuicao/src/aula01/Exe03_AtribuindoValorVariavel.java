c876de4f15a8af893949f7cc987f80af371f1943
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:17:45 -0300

    Exe03_AtribuindoValorVariavel

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe03_AtribuindoValorVariavel.java
@@ -0,0 +1,16 @@
+package aula01;
+
+public class Exe03_AtribuindoValorVariavel {
+
+	public static void main(String[] args) {
+
+		// ATRIBUINDO VALORES NA VARIÁVEL
+		int x = 10;
+		double y = 45.3;
+		char c = 'c';
+		String nome = "Vitória";
+		boolean ePar = true;
+
+	}
+ 
+}
