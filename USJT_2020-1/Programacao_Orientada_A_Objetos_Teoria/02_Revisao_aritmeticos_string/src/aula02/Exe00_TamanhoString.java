5348512422e54fcc5c17559e0d87f20a77c57730
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:19:36 -0300

    Exe00_TamanhoString

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/02_Revisao_aritmeticos_string/src/aula02/Exe00_TamanhoString.java
@@ -0,0 +1,15 @@
+package aula02;
+
+public class Exe00_TamanhoString {
+
+	public static void main(String[] args) {
+		
+		//RETORNANDO O TAMANHO DA STRING
+		//NOTE QUE O MÉTODO '.length()' CONVERTE O 'TIPO STRING' PARA UM INTEIRO:
+		int s = "87654321".length();
+		
+		System.out.println(s);
+		
+	}
+
+}
