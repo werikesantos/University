8efc27ad4c20717b36135fafd765a6ccd45be939
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:30:41 -0300

    Classe TelaLoginBorder

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/10_Interface_grafica/CódigosJFrame/aula_teoria/TelaLoginBorder.java
@@ -0,0 +1,75 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+
+public class TelaLoginBorder extends JFrame implements ActionListener{
+
+   private JTextField txtNome, txtSenha;
+   private JLabel lblNome, lblSenha;
+   private JButton btnOk, btnCancela;
+   
+   public TelaLoginBorder(){
+      super("Login");
+      txtNome = new JTextField(15);
+      txtSenha = new JTextField(15);
+      lblNome = new JLabel("Nome");
+      lblSenha = new JLabel("Senha");
+      btnOk = new JButton("Ok");
+      btnCancela = new JButton("Cancela");
+      
+      Container caixa = getContentPane();
+      caixa.setLayout(new BorderLayout());
+      
+      
+      add(lblNome, BorderLayout.NORTH);
+      add(txtNome, BorderLayout.WEST);
+      add(lblSenha, BorderLayout.CENTER);
+      add(txtSenha, BorderLayout.EAST);
+      add(btnOk, BorderLayout.SOUTH);
+      //add(btnCancela);
+      
+      btnOk.addActionListener(this);
+      btnCancela.addActionListener(this);
+      
+      setVisible(true);
+      //setResizable(false);
+      setSize(259,130);
+      setLocationRelativeTo(null);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+   
+   public void actionPerformed(ActionEvent ae){
+      if(ae.getSource() == btnCancela){
+         System.exit(0);
+      } else if (ae.getSource() == btnOk){
+         String usuario = "admin";
+         String senha = "123";
+         
+         String usuarioDigitado = txtNome.getText();
+         String senhaDigitada = txtSenha.getText();
+         
+         if(usuario.equals(usuarioDigitado) &&
+            senha.equals(senhaDigitada)){
+            JOptionPane.showMessageDialog(this, "Seja bem vindo");
+            System.exit(0);
+         } else {
+            JOptionPane.showMessageDialog(this, "Usuário/senha inválidos.\nTente estes que acabei de escrever para você, amigo.",
+               "Login", JOptionPane.ERROR_MESSAGE);
+            txtNome.setText("admin");
+            txtSenha.setText("123");
+               
+         }
+      }
+   
+   }
+
+   public static void main(String[] args){
+      new TelaLoginBorder();
+   }
+}
