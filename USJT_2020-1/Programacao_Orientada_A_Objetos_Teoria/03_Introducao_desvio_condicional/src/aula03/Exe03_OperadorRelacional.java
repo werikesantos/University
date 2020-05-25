8a10de1090e5f432905478e07abebeed10d0feb5
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:20:58 -0300

    Exe03_OperadorRelacional

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/03_Introducao_desvio_condicional/src/aula03/Exe03_OperadorRelacional.java
@@ -0,0 +1,21 @@
+package aula03;
+
+public class Exe03_OperadorRelacional {
+
+	public static void main(String[] args) {
+
+		/* EXEMPLOS DE OPERADORES RELACIONAIS:
+		 * 
+		 * > maior
+		 * >= maior ou igual
+		 * < menor
+		 * <= menor ou igual
+		 * == igual
+		 * != diferente
+		 * 
+		 */
+		
+
+	}
+
+}
