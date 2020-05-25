7a58ea1904826cfbfd756251d4391b00c630b747
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:17:33 -0300

    Exe02_DeclarandoVariaveis

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe02_DeclarandoVariaveis.java
@@ -0,0 +1,14 @@
+package aula01;
+
+public class Exe02_DeclarandoVariaveis {
+
+	public static void main(String[] args) {
+
+		//DECLARANDO VARIÁVEIS
+		int x;
+		double y;
+		String nome;
+
+	}
+
+}
