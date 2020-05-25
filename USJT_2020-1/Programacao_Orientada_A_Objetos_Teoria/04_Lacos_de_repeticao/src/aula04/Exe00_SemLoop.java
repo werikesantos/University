4cfef308a6e1606273568a96e7ff7cc702e6ce23
Werikesantos <werike.ms@gmail.com>
2020-05-25 16:22:14 -0300

    Exe00_SemLoop

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/04_Lacos_de_repeticao/src/aula04/Exe00_SemLoop.java
@@ -0,0 +1,27 @@
+package aula04;
+
+public class Exe00_SemLoop {
+
+	public static void main(String[] args) {
+
+		/* EXEMPLO SEM LOOP
+		 * 
+		 * Considere a seguinte situação:
+		 * Deseja - se escrever na tela os números de 1 a 10. 
+		 * Sem o uso laços de repetição, o código ficaria assim: 
+		 * 
+		 */
+
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("10");
+	}
+
+}
