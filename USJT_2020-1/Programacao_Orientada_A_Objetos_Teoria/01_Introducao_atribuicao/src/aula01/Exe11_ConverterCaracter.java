b258e58812465a91a1d1ffd1e4f4f502bb81a297
Werikesantos <werike.ms@gmail.com>
2020-06-30 22:57:31 -0300

    Converter Caracter

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe11_ConverterCaracter.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe11_ConverterCaracter.java
@@ -7,10 +7,11 @@ public class Exe11_ConverterCaracter {
 	public static void main(String[] args) {
 
 		//PEGANDO UM CARACTER NA STRING:
-		char w = JOptionPane.showInputDialog("Digite uma palavra:").charAt(1);
+		char caracter = JOptionPane.showInputDialog("Digite uma palavra:").charAt(0); 
+		//.charAt(1) DENTRO DO PARÊNTESE, COLOQUE A POSIÇÃO DO CARACTER QUE DESEJA PEGAR.
 								
 		//MOSTRANDO O RESULTADO:
-		JOptionPane.showMessageDialog(null, "Caracter na posição (1): " + w);
+		JOptionPane.showMessageDialog(null, "O primeiro Caracter: \n( " + caracter + " )");
 
 	}
 
