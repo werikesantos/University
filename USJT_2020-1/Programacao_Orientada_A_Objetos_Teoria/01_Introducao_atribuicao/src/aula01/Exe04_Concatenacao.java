f96872dd075ab085e202fce3f9472e2558f22c0c
Werikesantos <werike.ms@gmail.com>
2020-06-30 22:59:06 -0300

    Concatenação

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe04_Concatenacao.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe04_Concatenacao.java
@@ -8,9 +8,9 @@ public class Exe04_Concatenacao {
 		int g = 10;
 		String x = "Bom";
 		
-		x = x + " Dia"; //o conteúdo de x agora é "Bom Dia"
+		x = x + " Dia"; //O CONTEÚDO DE 'x' AGORA É "Bom Dia"
 		
-		x = x + ", " + g; //agora x vale "Bom Dia, 10"
+		x = x + ", " + g; //AGORA 'x' VALE "Bom Dia, 10"
 		
 		//MOSTRANDO O RESULTADO NO CONSOLE
 		System.out.println(x);
