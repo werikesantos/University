fcc75bf95ce4718c275c27d4dbbf56ebcc2508f8
Werikesantos <werike.ms@gmail.com>
2020-06-30 22:51:32 -0300

    Área do Retangulo

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/Exe02_AreaRetangulo.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/Exe02_AreaRetangulo.java
@@ -6,7 +6,7 @@ public class Exe02_AreaRetangulo {
 
 	public static void main(String[] args) {
 		
-		/* EXERCICIO 02
+		/* EXERCÍCIO 02
 		 * 
 		 * Crie um algoritmo para calcular a áre de um retangulo, com base nas medidas de sua base e de sua altura.
 		 * 
