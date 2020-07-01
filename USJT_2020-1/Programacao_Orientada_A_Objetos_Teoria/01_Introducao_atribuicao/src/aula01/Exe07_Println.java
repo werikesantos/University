d1275e229eb785403f410779496ca18cca173c3f
Werikesantos <werike.ms@gmail.com>
2020-06-30 22:58:25 -0300

    Imprimir no terminal

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe07_Println.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe07_Println.java
@@ -4,8 +4,9 @@ public class Exe07_Println {
 
 	public static void main(String[] args) {
 		
-		int x = 8;
-		int y = 1993;
+		int x = 8, y = 1993; //QUANDO A VARIÁVEL É DO MESMO TIPO, PODE SER DECLARADO DESTA FORMA, OU COMO O EXEMPLO ABAIXO!
+		//int y = 1993;
+		
 		//EXEMPLO DE EXIBIÇÃO NO CONSOLE
 		System.out.println("Erro do sistema");
 		
