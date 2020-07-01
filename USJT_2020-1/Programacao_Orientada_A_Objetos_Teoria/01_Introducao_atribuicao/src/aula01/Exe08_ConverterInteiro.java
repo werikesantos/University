25aa6c8c559f7ba5110451796fac3ff3a11129e4
Werikesantos <werike.ms@gmail.com>
2020-06-30 22:57:53 -0300

    Converter Inteiro

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe08_ConverterInteiro.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe08_ConverterInteiro.java
@@ -6,7 +6,7 @@ public class Exe08_ConverterInteiro {
 
 	public static void main(String[] args) {
 		
-		//CONVERTENDO A STRING PARA UM INTEIRO:
+		//CONVERTENDO 'JOptionPane' (QUE GUARDA SOMENTE VARIÁVEIS DO TIPO STRING) PARA UM TIPO INTEIRO (int).
 		int x = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor INTEIRO:"));
 		
 		//MOSTRANDO O RESULTADO:
