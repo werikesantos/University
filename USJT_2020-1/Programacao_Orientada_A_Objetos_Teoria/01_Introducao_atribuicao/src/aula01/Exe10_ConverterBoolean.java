06c30ac7988cabd19f51efae1bc3e343eade70e6
Werikesantos <werike.ms@gmail.com>
2020-06-30 22:57:42 -0300

    Converter Boolean

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe10_ConverterBoolean.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/aula01/Exe10_ConverterBoolean.java
@@ -6,7 +6,7 @@ public class Exe10_ConverterBoolean {
 
 	public static void main(String[] args) {
 
-		// CONVERTENDO A STRING PARA UM BOOLEAN:
+		// CONVERTENDO A STRING PARA O TIPO BOOLEAN:
 		boolean z = Boolean.parseBoolean(JOptionPane.showInputDialog("Digite 'true' ou 'false':"));
 
 		// MOSTRANDO O RESULTADO:
