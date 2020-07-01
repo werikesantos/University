f66e676788681f1c71bb3d84332d1e76847b3803
Werikesantos <werike.ms@gmail.com>
2020-06-30 23:00:44 -0300

    Não finalizado

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/exercicio_08.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/exercicio_08.java
@@ -6,7 +6,8 @@ public class exercicio_08 {
 
 	public static void main(String[] args) {
 			
-		/* EXERCICIO 07 - FAZENDO!!!
+		/* NÃO FINALIZADO!!!
+		 * EXERCÍCIO 08 - FAZENDO!!!
 		 * 
 		 * Entrar com um número e imprimir o logaritmo desse número na base 10.
 		 * 
@@ -14,10 +15,10 @@ public class exercicio_08 {
 		
 		String sAngulo = JOptionPane.showInputDialog("Digite um angulo em graus:");
 		
-		int angulo = Integer.parseInt(sAngulo);
-		
-		double a = Math.toRadians(angulo);
+		double angulo = Double.parseDouble(sAngulo);
 
+		double a = Math.toRadians(angulo);
+		
 		double seno = Math.sin(a);
 		double cosseno = Math.cos(a);
 		double tangente = Math.tan(a);
