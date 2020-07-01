79af6c466fc49776cb6c190d0754418855c7d3b1
Werikesantos <werike.ms@gmail.com>
2020-06-30 22:55:22 -0300

    Update exercicio_07.java

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/exercicio_07.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/02_OperadoresAritmeticosExpressoesAritmeticasEStrings/src/aula02/exercicio_07.java
@@ -1,10 +1,25 @@
 package aula02;
 
+import javax.swing.JOptionPane;
+
 public class exercicio_07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/* EXERCÍCIO O7
+		 * 
+		 * NÃO FINALIZADO!!!
+		 * Entrar com um ângulo em graus e imprimir: seno, cosseno, tangente, secante, cossecante e cotangente.
+		 * 
+		 */
+		
+		String dado = JOptionPane.showInputDialog("Digite o ângulo em graus:");
+		
+		Double valor = Double.parseDouble(dado);
+		
+		Double angulo = Math.sin(Math.PI/2);
+		
+		System.out.println(angulo);
 	}
 
 }
