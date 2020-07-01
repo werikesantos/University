2f145c670961b731b15bc903e09cac3f9992e813
Werikesantos <werike.ms@gmail.com>
2020-06-30 23:03:29 -0300

    Classe Principal

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Teoria/01_Introducao_atribuicao/src/exemploPOO1/Principal.java
@@ -0,0 +1,63 @@
+package exemploPOO1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 1) Crie um algoritmo que leia três palavras, separadamente, e as apresenta na tela na ordem
+		 * contrária em que foram digitadas, separadas por espaço.
+		 * 
+		 */
+		
+		//One one = new One();
+		//one.ordemContraria();
+		
+		/*
+		 * 2) Crie um algoritmo que leia um número inteiro, armazene-o em uma variável do tipo int e
+		 * depois escreva na tela: “O valor do número digitado é: ” e o valor do número.
+		 * 
+		 */
+		
+		//Two two = new Two();	
+		//two.imprimir();
+		
+		/*
+		 * 3) Crie um algoritmo que leia 3 números que representem uma data, respectivamente dia,
+		 * mês e ano. Depois imprima a data usando o formato dia/mês/ano.
+		 * 
+		 */
+		
+		//Three three = new Three();		
+		//three.data();
+		
+		/*
+		 * 4) Solicite ao usuário que digite seu peso no formato real. Depois imprima “O peso 
+		 * informado foi “ + peso + “ kg.”.
+		 * 
+		 */
+		
+		//Four four = new Four();		
+		//four.seuPeso();
+		
+		/*
+		 * 5) Ler dez caracteres e imprimir a palavra formada por eles.
+		 * 
+		 */
+		
+		//Five five = new Five();		
+		//five.imprimirPalavra();
+		
+		/*
+		 * 6) Leia 4 números inteiros, representando milhar, centena, dezena e unidade. Use o tipo
+		 * caractere para garantir que não seja digitado mais de um número por vez. Depois concatene
+		 * estes caracteres armazenando-os em uma String (lembre-se de que o jeito mais fácil de
+		 * transformar algo em String é concatenando-o com uma String vazia “”). Imprima o resultado.
+		 * 
+		 */
+		
+		Six six = new Six();		
+		six.imprimir();
+	}
+
+}
