9d874037187f09fc6af4954db0d993137ca11d82
Werikesantos <werike.ms@gmail.com>
2020-07-07 20:08:16 -0300

    Exemplo 00 (while) - atividade utilizando os laços

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/04_Lacos/src/aula04/Exemplo00_While.java
@@ -0,0 +1,29 @@
+package aula04;
+
+import javax.swing.JOptionPane;
+
+public class Exemplo00_While {
+
+	public static void main(String[] args) {
+
+		// RESOLUÇÃO DO EXERCÍCIO PROPOSTO DO KATA 1, UTILIZANDO OS 3 LAÇOS 'while', 'do-while' E 'for'
+		
+		//LAÇO 'while'
+		
+		int i = 1;
+
+		JOptionPane.showMessageDialog(null, "Resultado do Quadrado de números do 1 ao 20:", "Kata 1",JOptionPane.PLAIN_MESSAGE);
+		
+		while (i <= 20) {
+			
+			JOptionPane.showMessageDialog(null, "Número: " + i + " ao quadrado é = " + (i * i), "Kata 1", JOptionPane.PLAIN_MESSAGE);
+			
+			i = i + 1; //i++
+			
+		}
+		
+		System.exit(0);
+
+	}
+
+}
