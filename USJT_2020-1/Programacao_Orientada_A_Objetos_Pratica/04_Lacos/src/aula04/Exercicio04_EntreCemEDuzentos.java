b9c01f46002a6c7d20ec63132359039aaf46d4f7
Werikesantos <werike.ms@gmail.com>
2020-07-09 01:52:42 -0300

    Entre Cem e Duzentos (while - do_while - for)

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/04_Lacos/src/aula04/Exercicio04_EntreCemEDuzentos.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/04_Lacos/src/aula04/Exercicio04_EntreCemEDuzentos.java
@@ -16,24 +16,26 @@ public class Exercicio04_EntreCemEDuzentos {
 		Exercicio04_EntreCemEDuzentos exercicio04_EntreCemEDuzentos = new Exercicio04_EntreCemEDuzentos();
 		
 		exercicio04_EntreCemEDuzentos._while_EntreCemEDuzentos();
-
+		
+		exercicio04_EntreCemEDuzentos._do_while_EntreCemEDuzentos();
+		
+		exercicio04_EntreCemEDuzentos._for_EntreCemEDuzentos();
+	
 	}
 	
 	public void _while_EntreCemEDuzentos() {
 		
-		int a = 0;
-		int b = 1;
+		int a;
+		int b = 0;
 		
 		a = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:"));
 		
-		while(a > 0) {
+		while(a != 0) {
 			
 			if((a >= 100) && (a <= 200)) {
 				
 				//VARIÁVEL CONTADORA DE PROCESSOS
 				b++;
-				
-				a = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:"));
 
 			}
 			
@@ -42,6 +44,59 @@ public class Exercicio04_EntreCemEDuzentos {
 		}
 
 		JOptionPane.showMessageDialog(null, "Total de vezes em que números entre 100 e 200 foram digitados:\n " + b);
+		
+	}
+	
+	public void _do_while_EntreCemEDuzentos() {
+
+		int a;
+		int b = 0;
+		
+		a = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:", "Digite zero (0) para finalizar!", JOptionPane.PLAIN_MESSAGE));
+		
+		if(a != 0) {
+			
+			do {
+			
+				if((a >= 100) && (a <= 200)) {
+				
+					b++;
+				
+				}
+			
+				a = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:"));		
+			
+			}while(a > 0);
+		
+			JOptionPane.showMessageDialog(null, "Total de vezes em que números entre 100 e 200 foram digitados:\n" + b);
+		
+		}
+		
+			JOptionPane.showMessageDialog(null, "O Processo é finalizado ao digitar o número zero (0)!");
+	
+	}
+	
+	public void _for_EntreCemEDuzentos() {
+		
+		int b = 0;
+		int c;
+		
+		int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:", "Digite zero (0) para finalizar!", JOptionPane.PLAIN_MESSAGE));
+		
+		for(c = 1; a != 0; c++) {
+			
+			if((a >= 100) && (a <= 200)) {
+				
+				//VARIÁVEL CONTADORA DE PROCESSOS
+				b++;
+
+			}
+			
+			a = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:"));
+			
+		}
+		
+		System.out.println("Foram digitados um total de " + c +" números." + "\nTotal de vezes em que números entre 100 e 200 foram digitados:\n" + b);
 		
 	}
 
