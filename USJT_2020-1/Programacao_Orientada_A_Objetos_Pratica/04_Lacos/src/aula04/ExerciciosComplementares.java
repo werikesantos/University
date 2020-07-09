17ebcafefa0c933b1594df6d49f4194d56d640d9
Werikesantos <werike.ms@gmail.com>
2020-07-09 01:53:26 -0300

    Em processo

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/04_Lacos/src/aula04/ExerciciosComplementares.java
@@ -0,0 +1,24 @@
+package aula04;
+
+public class ExerciciosComplementares {
+
+	public static void main(String[] args) {
+
+		ExerciciosComplementares exerciciosComplementares = new ExerciciosComplementares();
+		
+		exerciciosComplementares.atividade5();
+		
+	}
+	
+	public void atividade5() {
+		
+		/* ATIVIDADE 5
+		 * 
+		 * Entrar com nomes enquanto forem diferentes de FIM e imprimir cada nome digitado.
+		 * 
+		 */
+		
+		
+	}
+
+}
