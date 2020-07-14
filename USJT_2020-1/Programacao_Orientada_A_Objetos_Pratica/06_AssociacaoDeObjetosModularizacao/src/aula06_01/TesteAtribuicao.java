6b6d8f77e0ae32258aa2f95b8c0922e4dc9a6cd8
Werikesantos <werike.ms@gmail.com>
2020-07-14 19:02:44 -0300

    Classe Teste Atribuição

--- USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/06_AssociacaoDeObjetosModularizacao/src/aula06_01/TesteAtribuicao.java
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/06_AssociacaoDeObjetosModularizacao/src/aula06_01/TesteAtribuicao.java
@@ -5,9 +5,10 @@ import javax.swing.JOptionPane;
 public class TesteAtribuicao {
 
 	public static void main(String[] args) {
-		
+			
 		//SOLICITANDO DADOS
-		String nomeProfessor = JOptionPane.showInputDialog(null, "Digite o nome:","CADASTRO DE PROFESSOR",JOptionPane.PLAIN_MESSAGE);
+		String nomeProfessor;
+		nomeProfessor = JOptionPane.showInputDialog(null, "Digite o nome:","CADASTRO DE PROFESSOR",JOptionPane.PLAIN_MESSAGE);
 		int idadeProfessor = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a idade:","CADASTRO DE PROFESSOR",JOptionPane.PLAIN_MESSAGE));
 		String nomeDisciplina = JOptionPane.showInputDialog(null, "Matéria:","CADASTRO DE DISCIPLINA",JOptionPane.PLAIN_MESSAGE);
 		boolean praticaDisciplina = Boolean.parseBoolean(JOptionPane.showInputDialog(null, "true OR false","MATÉRIA PRÁTICA:",JOptionPane.PLAIN_MESSAGE));
