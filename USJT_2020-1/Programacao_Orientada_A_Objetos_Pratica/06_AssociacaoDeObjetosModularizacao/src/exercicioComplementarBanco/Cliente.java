0a9a8d6e5df804b22ad1c8e10e46d06139ee5cf1
Werikesantos <werike.ms@gmail.com>
2020-08-10 21:30:47 -0300

    Projeto de um Banco Financeiro - Classe Cliente

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/06_AssociacaoDeObjetosModularizacao/src/exercicioComplementarBanco/Cliente.java
@@ -0,0 +1,51 @@
+package exercicioComplementarBanco;
+
+public class Cliente {
+
+	/* A.
+	 * 
+	 * A classe Cliente possui os atributos nome e cpf, ambos do tipo String, e um atributo conta
+	 * do tipo ContaCorrente. Crie um construtor que recebe os atributos como parametros e os
+	 * métodos de acesso e os modificadores.
+	 * 
+	 */
+	
+	//ATRIBUTOS
+	private String nome;
+	private String cpf;
+	private ContaCorrente conta;
+	
+	//CONTRUTOR
+	public Cliente(String nome, String cpf, ContaCorrente contaCorrente) {
+		
+		this.nome = nome;
+		this.cpf = cpf;
+		this.conta = contaCorrente;
+		
+	}
+	
+	//GETTER AND SETTER
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public ContaCorrente getConta() {
+		return conta;
+	}
+
+	public void setConta(ContaCorrente conta) {
+		this.conta = conta;
+	}
+}
