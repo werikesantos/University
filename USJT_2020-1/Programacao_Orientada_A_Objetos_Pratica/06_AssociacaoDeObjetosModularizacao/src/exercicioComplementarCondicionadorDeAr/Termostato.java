aaff6397a311556ea5efbbcd155b979190d9eaec
Werikesantos <werike.ms@gmail.com>
2020-08-10 21:32:14 -0300

    Projeto de Condicionador de AR - Classe Termostato

---
+++ USJT_2020-1/Programacao_Orientada_A_Objetos_Pratica/06_AssociacaoDeObjetosModularizacao/src/exercicioComplementarCondicionadorDeAr/Termostato.java
@@ -0,0 +1,22 @@
+package exercicioComplementarCondicionadorDeAr;
+
+public class Termostato {
+	
+	private int temperatura;
+	
+	//CONSTRUTOR
+	public Termostato() {
+		
+		this.temperatura = 20;
+		
+	}
+
+	//GETTER AND SETTER
+	public int getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(int temperatura) {
+		this.temperatura = temperatura;
+	}
+}
